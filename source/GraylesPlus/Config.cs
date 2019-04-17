a020d9606715c10d9506d819e39336bc2c2968bc
eric vz <git@base10.org>
2019-04-16 20:09:46 -0500

    start working on basic config and tests

--- source/GraylesPlus/Config.cs
+++ source/GraylesPlus/Config.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.IO;
 
 namespace GraylesPlus
 {
     public class Config
     {
+        private readonly string _graylesRoot;
+        private readonly string _starboundRoot;
+
+        public Config(): this(Directory.GetCurrentDirectory(), "") {}
+
+        public Config(string graylesRoot, string starboundRoot){
+            this._graylesRoot = graylesRoot;
+            this._starboundRoot = starboundRoot;
+        }
+
+        public Config With(string graylesRoot = null, string starboundRoot = null) => new Config(
+                graylesRoot: graylesRoot ?? this._graylesRoot,
+                starboundRoot: starboundRoot ?? this._starboundRoot
+            );
+
+        public string GraylesRoot { get {
+            return this._graylesRoot;
+        }}
+
+        public string ModRoot { get {
+            return Path.Combine(this.GraylesRoot, "mods");
+        }}
+
+        public string StorageRoot { get {
+            return Path.Combine(this.GraylesRoot, "storage");
+        }}
+
+        public string StarboundRoot { get {
+            return this._starboundRoot;
+        }}
     }
 }
--- source/GraylesPlusTests/ConfigTests.cs
+++ source/GraylesPlusTests/ConfigTests.cs
@@ -13,5 +13,33 @@ namespace GraylesPlusTests
             var config = new g.Config();
             Assert.True(config != null);
         }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(null,"D")]
+        [InlineData("C",null)]
+        [InlineData("C","D")]
+        public void TestDirectoryChanges(string grayles, string sb){
+            var config = new g.Config();
+            var config2 = config.With(grayles,sb);
+
+            Assert.NotSame(config, config2);
+
+            if(grayles == null){
+                Assert.Equal(config.GraylesRoot, config2.GraylesRoot);
+            } else {
+                Assert.Equal(grayles, config2.GraylesRoot);
+                Assert.NotEqual(config.GraylesRoot, config2.GraylesRoot);
+            }
+
+            if(sb == null){
+                Assert.Equal(config.StarboundRoot, config2.StarboundRoot);
+            } else {
+                Assert.Equal(sb, config2.StarboundRoot);
+                Assert.NotEqual(config.StarboundRoot, config2.StarboundRoot);
+            }
+
+        }
+
     }
 }
