24cdc7a185aa43279c000dad4c4d7253ea3cce05
eric vz <git@base10.org>
2019-04-19 08:31:28 -0500

    Start work on gui

---
+++ source/GraylesGui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using g = GraylesPlus;
+
+namespace GraylesGui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public g.Config Config => g.Config.Load();
+
+        public g.Mods Mods => new g.Mods(this.Config);
+
+        public g.Starbound Starbound => new g.Starbound(this.Config);
+
+        public string StarboundFound => this.Starbound.StarboundExecutableFolder != null ? "Yes" : "No";
+
+        public string GraylesSet => this.Config.GraylesRoot != null ? $"Yes" : "No";
+
+        public string ModsDownloaded => this.Mods.Downloaded ? $"Yes, {Mods.TargetVersion}" : "No";
+
+        public string ModsInstalled => this.Mods.Installed ? $"Yes, {Mods.InstalledVersion}" : "No";
+    }
+}
