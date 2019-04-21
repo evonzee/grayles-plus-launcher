f6fd84d662eb37f3dc72a10e3490e7540fb0bc52
eric vz <evonzee@base10.org>
2019-04-21 11:06:35 -0500

    add hashcode support

--- source/GraylesPlus/ModpackVersion.cs
+++ source/GraylesPlus/ModpackVersion.cs
@@ -1,13 +1,15 @@
-﻿namespace GraylesPlus
+namespace GraylesPlus
 {
     public class ModpackVersion
     {
         public readonly string VersionNumber;
+        public readonly string HashCode;
         public readonly string DownloadUrl;
 
-        public ModpackVersion(string version, string url)
+        public ModpackVersion(string version, string hashcode, string url)
         {
             this.VersionNumber = version;
+            this.HashCode = hashcode;
             this.DownloadUrl = url;
         }
     }
