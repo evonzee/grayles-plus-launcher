48934a92d4bfe4d7c1c81a4db8d42f42301783e6
eric vz <evonzee@base10.org>
2019-04-21 10:49:27 -0500

    add starting code for online update

---
+++ source/GraylesPlus/ModpackVersion.cs
@@ -0,0 +1,14 @@
+﻿namespace GraylesPlus
+{
+    public class ModpackVersion
+    {
+        public readonly string VersionNumber;
+        public readonly string DownloadUrl;
+
+        public ModpackVersion(string version, string url)
+        {
+            this.VersionNumber = version;
+            this.DownloadUrl = url;
+        }
+    }
+}
---
+++ source/GraylesPlus/OnlineUpdate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraylesPlus
+{
+    public class OnlineUpdate
+    {
+
+
+        private readonly Config _config;
+
+        #region Constructors and transformers
+
+        public OnlineUpdate(Config config, IEnumerable<ModpackVersion> versions = null, string updateUrl = null)
+        {
+            this._config = config;
+            List<ModpackVersion> newVersions = new List<ModpackVersion>();
+            newVersions.AddRange(versions ?? new List<ModpackVersion>());
+            this.AvailableVersions = newVersions.AsReadOnly();
+            this.UpdateUrl = updateUrl ?? this._config.UpdateUrl;
+        }
+
+        public OnlineUpdate With(Config config = null, List<ModpackVersion> versions = null, string updateUrl = null) => new OnlineUpdate(
+                config: config ?? this._config,
+                versions: versions ?? this.AvailableVersions,
+                updateUrl: updateUrl ?? this.UpdateUrl
+            );
+
+        #endregion
+
+
+        #region Public properties
+
+        public IEnumerable<ModpackVersion> AvailableVersions { get; private set; }
+
+        public string UpdateUrl { get; private set; }
+
+        #endregion
+
+
+
+        #region Activities this class can perform
+
+        public static OnlineUpdate Fetch(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        #endregion
+
+    }
+}
