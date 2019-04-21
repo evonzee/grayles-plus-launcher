859743284f040f3c588b7c259daa9014cf0d751d
eric vz <evonzee@base10.org>
2019-04-21 10:49:39 -0500

    refactor config to have smarter defaulting

--- source/GraylesPlus/Config.cs
+++ source/GraylesPlus/Config.cs
@@ -11,17 +11,20 @@ namespace GraylesPlus
 
         private readonly string _graylesRoot;
         private readonly string _starboundRoot;
+        private readonly string _updateUrl;
 
-        public Config(): this(AppDirectory, "") {}
+        public Config(): this(null, null, null) {}
 
-        public Config(string graylesRoot, string starboundRoot){
-            this._graylesRoot = graylesRoot;
-            this._starboundRoot = starboundRoot;
+        public Config(string graylesRoot, string starboundRoot, string updateUrl){
+            this._graylesRoot = graylesRoot ?? AppDirectory;
+            this._starboundRoot = starboundRoot ?? "";
+            this._updateUrl = updateUrl ?? "https://raw.githubusercontent.com/evonzee/grayles-plus-launcher/master/.updates.json";
         }
 
-        public Config With(string graylesRoot = null, string starboundRoot = null) => new Config(
+        public Config With(string graylesRoot = null, string starboundRoot = null, string updateUrl = null) => new Config(
                 graylesRoot: graylesRoot ?? this._graylesRoot,
-                starboundRoot: starboundRoot ?? this._starboundRoot
+                starboundRoot: starboundRoot ?? this._starboundRoot,
+                updateUrl: updateUrl ?? this._updateUrl
             );
 
         #endregion
@@ -48,6 +51,9 @@ namespace GraylesPlus
             return this._starboundRoot;
         }}
 
+        public string UpdateUrl { get; internal set; }
+
+
         #endregion
 
         #region Save and load from disk
@@ -56,15 +62,15 @@ namespace GraylesPlus
         private static string ConfigFile { get {return Path.Combine(AppDirectory, "config.json"); } }
 
         public static Config Load() {
-            var spec = new { graylesRoot = AppDirectory, starboundRoot = "" };
+            var spec = new { graylesRoot = AppDirectory, starboundRoot = "", updateUrl = "" };
             if(File.Exists(ConfigFile)) {
                 spec = JsonConvert.DeserializeAnonymousType(File.ReadAllText(ConfigFile), spec);
             }
-            return new Config(spec.graylesRoot, spec.starboundRoot);
+            return new Config(spec.graylesRoot, spec.starboundRoot, spec.updateUrl);
         }
 
         public static void Save(Config config){
-            var json = new { graylesRoot = config.GraylesRoot, starboundRoot = config.StarboundRoot };
+            var json = new { graylesRoot = config.GraylesRoot, starboundRoot = config.StarboundRoot, updateUrl = config.UpdateUrl };
             File.WriteAllText(ConfigFile, JsonConvert.SerializeObject(json));
         }
 
